b5682b76d9d771b90b2c17384ddca1d87ec9b697
Svetlana K <kov_sv@mail.ru>
2022-08-15 14:11:46 +1000

    First commit

---
+++ src/MagicBox.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class MagicBox<T> {
+    protected T[] items;
+
+    public MagicBox(int maxItems) {
+        this.items = (T[]) new Object[maxItems];
+    }
+
+    int filled = 0;
+
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                filled += 1;
+                return true;
+            }
+        }
+        return false;
+    }
+    Random random = new Random();
+
+    public T pick() {
+
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException("Коробка не заполнена, осталось заполнить " + (items.length - filled) + " ячеек.");
+    //            continue;
+            } else {
+                int randomInt = random.nextInt(items.length);
+                System.out.println("Коробка заполнена. Случайный элемент из коробки: " + items[randomInt]);
+                System.out.println();
+                return items[randomInt];
+            }
+        }
+        return null;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        MagicBox <String> box1 = new MagicBox<>(4);
+        box1.add("игра");
+        box1.add("кролик");
+        //box1.add("шляпа");
+        //box1.add("зеркало");
+        box1.pick();
+
+//        MagicBox <Integer> box2 = new MagicBox<>(5);
+//        box2.add(3);
+//        box2.add(1);
+//        box2.pick();
+    }
+}
