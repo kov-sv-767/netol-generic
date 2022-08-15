c99170fade49fd1f855344b8a50bed4cc135120e
Svetlana K <kov_sv@mail.ru>
2022-08-15 16:36:25 +1000

    Устранена ошибка кода в методе T pick

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -19,22 +19,25 @@ public class MagicBox<T> {
         }
         return false;
     }
-    Random random = new Random();
-    int randomInt;
-    public T pick() {
 
+
+    //int randomInt;
+    public T pick() {
+        Random random = new Random();
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
                 throw new RuntimeException("Коробка не заполнена, осталось заполнить " + (items.length - filled) + " ячеек.");
-    //            continue;
-            } else {
-                randomInt = random.nextInt(items.length);
+            }
+        }
+        for (int i = 0; i < items.length; i++){
+            if (items[i] != null){
+                int randomInt = random.nextInt(items.length);
                 System.out.println("Коробка заполнена. Случайный элемент из коробки: " + items[randomInt]);
                 System.out.println();
-
-            }return items[randomInt];
+                return items[randomInt];
+            }
         }
-        return items[randomInt];
+        return null;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ public class Main {
         box1.add("игра");
         box1.add("кролик");
         box1.add("шляпа");
-        //box1.add("зеркало");
+        box1.add("зеркало");
         box1.pick();
 
-//        MagicBox <Integer> box2 = new MagicBox<>(5);
-//        box2.add(3);
-//        box2.add(1);
-//        box2.pick();
+        MagicBox <Integer> box2 = new MagicBox<>(5);
+        box2.add(3);
+        box2.add(1);
+        box2.add(10);
+        box2.add(23);
+        box2.add(68);
+        box2.pick();
     }
 }
